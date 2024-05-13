0998cc19c0c9fe8057d8b83a13bee803032fac46
Andjanie <andjaniejasmine@gmail.com>
2024-05-13 18:50:42 +0700

    part3

---
+++ operator.js
@@ -0,0 +1,18 @@
+const hasil = 1 + 2;
+console.log(`hasil: ${hasil}`);
+
+const hasil2 = 1 - 2;
+console.log(`hasil2: ${hasil2}`);
+
+const hasil3 = 1 * 2;
+console.log(`hasil3: ${hasil3}`);
+
+const hasil4 = 1 / 2;
+console.log(`hasil4: ${hasil4}`);
+
+const hasil5 = 2 ** 3;
+console.log(`hasil5: ${hasil5}`);
+
+const hasil6 = 10 % 2;
+console.log(`hasil6: ${hasil6}`);
+
